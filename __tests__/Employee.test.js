8641485e392620c19b38729307418c38259093e8
Kimberly Salas <salask0104@gmail.com>
2021-05-29 06:59:09 -0700

    added initial files and worked on employee test case

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,32 @@
+const Employee = require('../lib/Employee')
+
+// constructive pieces
+
+test('check to see if class is an object', () => {
+    const employee = new Employee ('kim')
+
+    expect(typeof employee).toBe('object')
+});
+
+test('check for name', () => {
+    const name = new Employee ('kim')
+
+    expect(name.name).toBe('kim')
+});
+
+test('check for id', () => {
+    const id = new Employee ('kim', 24)
+
+    expect(id.id).toBe(24)
+
+});
+
+test('check for email', () => {
+    const email = new Employee ('kim', 24, 'e@email.com')
+
+    expect(email.email).toBe('e@email.com')
+
+});
+
+
+//Methods
---
+++ index.js
@@ -0,0 +1,31 @@
+// add all the const
+
+
+//start the application
+
+
+
+
+// this is where the questions will go
+//manager
+    //Name
+    //employee ID 
+    //email address
+    //office number
+
+//engineer
+    //Name
+    //ID
+    //email
+    //GitHub username
+    //back to the menu
+
+
+//intern
+    //Name
+    //ID
+    //email 
+    //School
+    //back to the menu
+
+//exit the app and the HTML is generated
