69054f83d7fd7f146b43140f91a8bb05f8723da5
Kimberly Salas <salask0104@gmail.com>
2021-05-30 14:50:30 -0700

    added the function that will generate the HTML page using the file system

--- index.js
+++ index.js
@@ -198,4 +198,31 @@ const startApp = () => {
 
     };
 
+
+    //this is the function that will generate HTML page file using the file system
+    const writeFile = data => {
+        fs.writeFile('./dist/index.html', data, err => {
+            // if there is an error 
+            if (err) {
+                console.log(err);
+                return;
+                // when the profile has been created 
+            } else {
+                console.log("Your team profile has been successfully created! Please check out the index.html")
+            }
+        })
+    };
+
+    addManager()
+        .then(addEmployee)
+        .then(teamMembers => {
+            return generateHTML(teamMembers);
+        })
+        .then(pageHTML => {
+            return writeFile(pageHTML);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
 }
