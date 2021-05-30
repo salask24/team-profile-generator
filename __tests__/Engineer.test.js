be47b12715abc360f03036132a94c8d51db969c7
Kimberly Salas <salask0104@gmail.com>
2021-05-30 07:17:18 -0700

    added to engineer test

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,22 +1,22 @@
 const Engineer = require('../lib/Engineer');
 
 test('Creates an Engineer object', () => {
-    const engineer = new Engineer('Engineer Name', '15', 'engineer@noemail.com', 'GitHubUsername')
+    const engineer = new Engineer('kim', '24', 'e@email.com', 'GithubUsername')
 
-    expect(engineer.employeeName).toEqual(expect.any(String));
-    expect(engineer.employeeID).toEqual(expect.any(String));
-    expect(engineer.employeeEmail).toEqual(expect.stringContaining('@'));
-    expect(engineer.gitHubUsername).toEqual(expect.any(String));
+    expect(engineer.name).toEqual(expect.any(String));
+    expect(engineer.id).toEqual(expect.any(String));
+    expect(engineer.email).toEqual(expect.stringContaining('@'));
+    expect(engineer.githubUsername).toEqual(expect.any(String));
 });
 
 test("Gets Engineer's GitHub username", () => {
-    const engineer = new Engineer('Engineer Name', '15', 'engineer@noemail.com', 'GitHubUsername')
+    const engineer = new Engineer('kim', '24', 'e@email.com', 'GithubUsername')
 
-    expect(engineer.getGitHub()).toEqual(expect.any(String));
+    expect(engineer.githubUsername()).toEqual(expect.any(String));
 });
 
 test("Gets Engineer's role", () => {
-    const engineer = new Engineer('Engineer Name', '15', 'engineer@noemail.com', 'GitHubUsername')
+    const engineer = new Engineer('kim', '24', 'e@email.com', 'GithubUsername')
 
     expect(engineer.getRole()).toEqual(expect.stringContaining('Engineer'));
 
