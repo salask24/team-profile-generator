6406107672ae0ccacf869f665a4f9597f2cfa01a
Kimberly Salas <salask0104@gmail.com>
2021-05-30 08:51:37 -0700

    added manager and intern tests and tweaked manager.js file

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,24 @@
-const 
+const Intern = require('../lib/Intern');
+
+test('Creates an Intern object', () => {
+    const intern = new Intern('Intern Name', '20', 'intern@noemail.com', 'internSchool')
+
+    expect(intern.name).toEqual(expect.any(String));
+    expect(intern.id).toEqual(expect.any(String));
+    expect(intern.email).toEqual(expect.stringContaining('@'));
+    expect(intern.internSchool).toEqual(expect.any(String));
+});
+
+test("Gets Intern's school", () => {
+    const intern = new Intern('Intern Name', '24', 'intern@email.com', 'internSchool')
+
+    expect(intern.getSchool()).toEqual(expect.any(String));
+});
+
+test("Gets Intern's role", () => {
+    const intern = new Intern('Intern Name', '24', 'intern@email.com', 'internSchool')
+
+    expect(intern.getRole()).toEqual(expect.stringContaining('Intern'));
+
+    console.log(intern);
+});
