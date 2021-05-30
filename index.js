3ac897969659d3a83fc9dc82c5e6510b3b371c02
Kimberly Salas <salask0104@gmail.com>
2021-05-30 09:04:20 -0700

    added files to index.js and started manager- adding information questions

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
 // add all the const
+const fs = require('fs');
 
+const inquirer = require('inquirer');
+
+const Manager = require('./lib/Manager');
+
+const Engineer = require('./lib/Engineer');
+
+const Intern = require('./lib/Intern');
+
+const generatePage = require('./src/page-template');
+
+const { writeFile, copyFile } = require('./util/generate-site');
+
+const teamMembers = [];
 
 //start the application
+const startApp = () => {
 
+    //questions for adding manager information
+    const addManager = () => {
+        return inquirer.prompt([
+            {
+                type: '',
+                name: '',
+                validate: answerInput => {
+                    if(answerInput){
+                        return true;
+                    } else {
+                        console.log('Please enter your team managers name.');
+                        return false;
+                    }
+                }
+            }
+        ]
+        )}
+
+
+}
 
 
 
